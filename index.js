531d8010af861f3bb5c8b7f4b2b34d58e249c5af
Alaivin <alaivin@yandex.ru>
2019-12-06 17:15:54 +0300

    feature: added play music

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 const scale = 50;
+const file = document.getElementById('audioFile');
+const audio = document.getElementById('audio');
+
+file.addEventListener('change', () => audio.play());
 
 const drawGrid = () => {
   context.clearRect(0, 0, canvas.width, canvas.height);
@@ -8,11 +12,12 @@ const drawGrid = () => {
   for (let i = 0; i <= canvas.width; i+= scale) {
     context.moveTo(i, 0);
     context.lineTo(i, canvas.height);
-  }
+  };
   for (let i = 0; i <= canvas.height; i+= scale) {
     context.moveTo(0, i);
     context.lineTo(canvas.width, i);
-  }
+  };
 context.stroke();
-}
+};
+
 drawGrid();
