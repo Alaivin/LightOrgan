f0f7ea7444d1c5da10b3aae8bb1418a14a6890eb
Alaivin <alaivin@yandex.ru>
2019-12-07 18:09:19 +0300

    feature: boombox implementation

--- index.js
+++ index.js
@@ -1,32 +1,69 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
-const scale = 50;
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+
 const file = document.getElementById('audioFile');
 const audio = document.getElementById('audio');
 
-const playMusic = () => {
-  audio.play();
-  const context = new AudioContext();
-  const src = context.createMediaElementSource(audio);
-  const analyser = context.createAnalyser();
-  src.connect(analyser);
-  analyser.connect(context.destination);
-}
+// const playMusic = () => {
+//   audio.play();
+//   const context = new AudioContext();
+//   const src = context.createMediaElementSource(audio);
+//   const analyser = context.createAnalyser();
+//   src.connect(analyser);
+//   analyser.connect(context.destination);
+// }
 
-file.addEventListener('change', playMusic);
+// file.addEventListener('change', playMusic);
 
-const drawGrid = () => {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.beginPath();
-  for (let i = 0; i <= canvas.width; i+= scale) {
-    context.moveTo(i, 0);
-    context.lineTo(i, canvas.height);
+
+const audioCtx = new AudioContext();
+const analyser = audioCtx.createAnalyser();
+
+
+const audioFile = new Audio();
+audioFile.src = './Music/believer.mp3';
+audioFile.autoplay = true;
+const audioSourceNode = audioCtx.createMediaElementSource(audioFile);
+
+
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+audioSourceNode.connect(analyser)
+analyser.connect(audioCtx.destination);
+
+context.clearRect(0, 0, canvasWidth, canvasHeight);
+
+const drawBoombox = () => {
+  const drawVisual = requestAnimationFrame(drawBoombox);
+
+  analyser.getByteFrequencyData(dataArray);
+
+  context.fillStyle = 'rgb(0, 0, 0)';
+  context.fillRect(0, 0, canvasWidth, canvasHeight);
+
+  const barWidth = 50;
+  const barHeight = 50;
+  let x = 5;
+  const position = {
+    x: 10,
+    y: 10
   };
-  for (let i = 0; i <= canvas.height; i+= scale) {
-    context.moveTo(0, i);
-    context.lineTo(canvas.width, i);
+
+  for (let i = 1; i < 144; i++) {
+    context.fillStyle = 'hsl(' + (i * 5) + ',' + (dataArray[i]) +'%' + ', 50%)';
+
+    if (i === 16) context.fillRect(position.x, position.y, barWidth, barHeight);
+
+    if (i % 16 === 0) {
+      position.y = position.y + barHeight + 5;
+      position.x = 10;
+    };
+
+    context.fillRect(position.x, position.y, barWidth, barHeight);
+    position.x += barWidth + 5;
   };
-context.stroke();
 };
 
-drawGrid();
+drawBoombox();
