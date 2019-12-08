8a17e2ca60ec246a7c4ec3f724d14e5c401e89a8
Alaivin <alaivin@yandex.ru>
2019-12-08 17:44:53 +0300

    feature: added custom color range

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 const submitBoomboxSizeButton = document.querySelector('.submit');
 const widthBarsInput = document.getElementById('width-number');
 const heightBarsInput = document.getElementById('height-number');
+const minColorInput = document.getElementById('min-color');
+const maxColorInput = document.getElementById('max-color');
 const boomboxSizeElement = document.getElementsByClassName('boombox-size')[0];
 const errorBox = document.getElementsByClassName('error-box')[0];
 const controlsWrapper = document.getElementsByClassName('controls-wrapper')[0];
+const sliders = [...document.getElementsByClassName('slider')];
 
-const getBarsNumbersValid = (widthNumber, heightNumber) => Number.isInteger(widthNumber) && Number.isInteger(heightNumber) && widthNumber > 4 && widthNumber < 21 && heightNumber > 4 && heightNumber < 21;
+sliders.forEach(slider => {
+    slider.addEventListener('change', event => {
+      const { value, name } = event.target;
+      const element = document.getElementById(name);
+      element.textContent = value;
+    })
+  }
+)
+
+const getBarsNumbersValid = (widthNumber, heightNumber, minColor, maxColor) => Number.isInteger(widthNumber) && Number.isInteger(heightNumber) && widthNumber > 4 && widthNumber < 21 && heightNumber > 4 && heightNumber < 21 && maxColor > minColor;
+
+const generateRandomColor = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 
 submitBoomboxSizeButton.addEventListener('click', event => {
   event.preventDefault();
   const widthNumber = Number(widthBarsInput.value);
   const heightNumber = Number(heightBarsInput.value);
-  const isBarsNumbersValid = getBarsNumbersValid(widthNumber, heightNumber);
+  const minColor = Number(minColorInput.value);
+  const maxColor = Number(maxColorInput.value);
+  const isBarsNumbersValid = getBarsNumbersValid(widthNumber, heightNumber, minColor, maxColor);
   if (isBarsNumbersValid) {
     boomboxSizeElement.classList.add("disable");
-    renderBoombox(widthNumber, heightNumber);
     errorBox.classList.add('disable');
     controlsWrapper.classList.remove('disable');
+    renderBoombox(widthNumber, heightNumber, minColor, maxColor);
   } else {
     errorBox.classList.remove('disable');
   };
 });
 
 
-const renderBoombox = (widthNumber, heightNumber) => {
+const renderBoombox = (widthNumber, heightNumber, minColor, maxColor) => {
   const canvas = document.querySelector('canvas');
   const context = canvas.getContext('2d');
   canvas.width = widthNumber * 50 + widthNumber * 5 + 5;
@@ -58,8 +75,10 @@ const renderBoombox = (widthNumber, heightNumber) => {
       gap: 5
     };
 
+    const color = generateRandomColor(minColor, maxColor)
+
     for (let i = 1; i < totalBarsNumber; i++) {
-      context.fillStyle = 'hsl(' + (i * 5) + ',' + dataArray[i] +'%' + ', 50%)';
+      context.fillStyle = 'hsl(' + color + ',' + dataArray[i] +'%' + ', 50%)';
 
       if (i === widthNumber) {
         context.fillRect(barData.positionX, barData.positionY, barData.width, barData.height);
