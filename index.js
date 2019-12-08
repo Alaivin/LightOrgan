930fdb0641284ae121354c22c15ca773c01b5d04
Alaivin <alaivin@yandex.ru>
2019-12-08 15:16:24 +0300

    feature: added custom boombox size

--- index.js
+++ index.js
@@ -1,48 +1,77 @@
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
-const canvasWidth = canvas.width;
-const canvasHeight = canvas.height;
-const audio = document.getElementById('audio');
+const submitBoomboxSizeButton = document.querySelector('.submit');
+const widthBarsInput = document.getElementById('width-number');
+const heightBarsInput = document.getElementById('height-number');
+const boomboxSizeElement = document.getElementsByClassName('boombox-size')[0];
+const errorBox = document.getElementsByClassName('error-box')[0];
 
-const audioCtx = new AudioContext();
-const analyser = audioCtx.createAnalyser();
-const audioSourceNode = audioCtx.createMediaElementSource(audio);
-const bufferLength = analyser.frequencyBinCount;
-const dataArray = new Uint8Array(bufferLength);
-audioSourceNode.connect(analyser)
-analyser.connect(audioCtx.destination);
+const getBarsNumbersValid = (widthNumber, heightNumber) => Number.isInteger(widthNumber) && Number.isInteger(heightNumber) && widthNumber > 4 && widthNumber < 21 && heightNumber > 4 && heightNumber < 21;
 
-const drawBoombox = () => {
-  const drawVisual = requestAnimationFrame(drawBoombox);
+submitBoomboxSizeButton.addEventListener('click', event => {
+  event.preventDefault();
+  const widthNumber = Number(widthBarsInput.value);
+  const heightNumber = Number(heightBarsInput.value);
+  const isBarsNumbersValid = getBarsNumbersValid(widthNumber, heightNumber);
+  if (isBarsNumbersValid) {
+    boomboxSizeElement.classList.add("disable");
+    renderBoombox(widthNumber, heightNumber);
+    errorBox.classList.add('disable');
+  } else {
+    errorBox.classList.remove('disable');
+  };
+});
 
-  analyser.getByteFrequencyData(dataArray);
 
-  context.fillStyle = 'rgb(0, 0, 0)';
-  context.fillRect(0, 0, canvasWidth, canvasHeight);
-  
-  const barData = {
-    positionX: 5,
-    positionY: 5,
-    width: 50,
-    height: 50,
-    gap: 5
-  }
+const renderBoombox = (widthNumber, heightNumber) => {
+  const canvas = document.querySelector('canvas');
+  const context = canvas.getContext('2d');
+  canvas.width = widthNumber * 50 + widthNumber * 5 + 5;
+  canvas.height = heightNumber * 50 + heightNumber * 5 + 5;
 
-  for (let i = 1; i < 144; i++) {
-    context.fillStyle = 'hsl(' + (i * 5) + ',' + dataArray[i] +'%' + ', 50%)';
+  const audio = document.getElementById('audio');
 
-    if (i === 16) {
-      context.fillRect(barData.positionX, barData.positionY, barData.width, barData.height);
-    }
+  const audioCtx = new AudioContext();
+  const analyser = audioCtx.createAnalyser();
+  const audioSourceNode = audioCtx.createMediaElementSource(audio);
 
-    if (i % 16 === 0) {
-      barData.positionY = barData.positionY + barData.height + barData.gap;
-      barData.positionX = barData.gap;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+  audioSourceNode.connect(analyser);
+  analyser.connect(audioCtx.destination);
+
+  const totalBarsNumber = widthNumber * heightNumber;
+
+  const drawBoombox = () => {
+    const drawVisual = requestAnimationFrame(drawBoombox);
+
+    analyser.getByteFrequencyData(dataArray);
+
+    context.fillStyle = 'rgb(0, 0, 0)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    const barData = {
+      positionX: 5,
+      positionY: 5,
+      width: 50,
+      height: 50,
+      gap: 5
     };
 
-    context.fillRect(barData.positionX, barData.positionY, barData.width, barData.height);
-    barData.positionX += barData.width + barData.gap;
-  };
-};
+    for (let i = 1; i < totalBarsNumber; i++) {
+      context.fillStyle = 'hsl(' + (i * 5) + ',' + dataArray[i] +'%' + ', 50%)';
 
-drawBoombox();
+      if (i === widthNumber) {
+        context.fillRect(barData.positionX, barData.positionY, barData.width, barData.height);
+      };
+
+      if (i % widthNumber === 0) {
+        barData.positionY = barData.positionY + barData.height + barData.gap;
+        barData.positionX = barData.gap;
+      };
+
+      context.fillRect(barData.positionX, barData.positionY, barData.width, barData.height);
+      barData.positionX += barData.width + barData.gap;
+    };
+  };
+
+  drawBoombox();
+}
